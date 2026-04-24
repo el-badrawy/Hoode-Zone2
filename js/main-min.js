27f1386a79fff6b237ab037a272c8e6492b63e6f
HOSSAM <hossamelbedrol@gmail.com>
2026-04-24 17:23:14 +0300

    edit main

--- js/main-min.js
+++ js/main-min.js
@@ -1,1 +1,40 @@
-const products=[{image:"hoodies/1.1.webp"},{image:"hoodies/2.1.webp"},{image:".hoodies/3.1.webp"},{image:".hoodies/4.1.webp"},{image:".t-shirt/1.1.webp"},{image:".t-shirt/2.1.webp"},{image:".t-shirt/3.1.webp"},{image:".pants/1.1.webp"},{image:".pants/2.1.webp"},{image:".pants/3.1.webp"}],carouse=document.getElementById("moreSell");let html="";products.forEach(((e,t)=>{html+=`\n  <div class="card">\n    <img src=${e.image} loading="lazy">\n  </div>\n  `})),carouse.innerHTML=html;const cards=Array.from(document.querySelectorAll(".card"));let interval,autoTimeout,current=1;function update(){cards.forEach(((e,t)=>{e.classList.remove("left","center","right","hidden","left2","right2"),t===current?e.classList.add("center"):t===current-1?e.classList.add("left"):t===current+1?e.classList.add("right"):e.classList.add("hidden")}))}function startAuto(){interval=setInterval((()=>{current++,current>=cards.length&&(current=0),update()}),2e3)}update(),startAuto(),loadLang(currentLang);
+const products = [
+    { image: "hoodies/1.1.webp" },
+    { image: "hoodies/2.1.webp" },
+    { image: "hoodies/3.1.webp" },
+    { image: "hoodies/4.1.webp" },
+    { image: "t-shirt/1.1.webp" },
+    { image: "t-shirt/2.1.webp" },
+    { image: "t-shirt/3.1.webp" },
+    { image: "pants/1.1.webp" },
+    { image: "pants/2.1.webp" },
+    { image: "pants/3.1.webp" },
+  ],
+  carouse = document.getElementById("moreSell");
+let html = "";
+(products.forEach((e, t) => {
+  html += `\n  <div class="card">\n    <img src=${e.image} loading="lazy">\n  </div>\n  `;
+}),
+  (carouse.innerHTML = html));
+const cards = Array.from(document.querySelectorAll(".card"));
+let interval,
+  autoTimeout,
+  current = 1;
+function update() {
+  cards.forEach((e, t) => {
+    (e.classList.remove("left", "center", "right", "hidden", "left2", "right2"),
+      t === current
+        ? e.classList.add("center")
+        : t === current - 1
+          ? e.classList.add("left")
+          : t === current + 1
+            ? e.classList.add("right")
+            : e.classList.add("hidden"));
+  });
+}
+function startAuto() {
+  interval = setInterval(() => {
+    (current++, current >= cards.length && (current = 0), update());
+  }, 2e3);
+}
+(update(), startAuto(), loadLang(currentLang));
